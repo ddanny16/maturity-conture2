23372dcf896fb6db337209304f1c5ee19d8e930a
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2022-10-15 22:29:05 -0700

    ...

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroImg from '../components/HeroImg'
+import Contact1 from "../assets/contact_us.jpg"
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      <p>
-        contact us
-      </p>
+      <HeroImg heading="Contact Us" text="Our Contacts & How to reach us" trc={Contact1} link1="Products" link2="Contact" link1a="/project" link2a="contact" />
+     
     </div>
   )
 }
