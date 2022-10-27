47830b56cb4cca041c0895f2f2f80c47609802f7
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2022-10-27 11:11:22 -0700

    ...

---
+++ src/layouts/Category.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Category.scss"
+
+const Category = () => {
+  return (
+    <>
+     <h1>categoryyy</h1>
+    </>
+  )
+}
+
+export default Category
--- src/routes/Products.js
+++ src/routes/Products.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import IntroImj5 from "../assets/aman.jpg"
 import HeroImg from '../components/HeroImg'
+import Category from "../layouts/Category"
 import Footer from '../components/Footers'
 
 
@@ -12,7 +13,7 @@ const Products = () => {
     <div>
       <Navbar />
       <HeroImg heading="Our Products" text="BE SURE TO CONTACT US FOR MORE" trc={IntroImj5} link1="Home" link2="Contact" link1a="/" link2a="contact" />
-      
+      <Category/>
       
       <Footer />
     </div>
